9be2d7db2c426c7509ce0cc1cc6e61e71ceb6799
Gustavo Espíndola <36246104+gustavao@users.noreply.github.com>
2021-09-22 09:48:22 -0500

    update

--- widget.js
+++ widget.js
@@ -1,8 +1,7 @@
 <script type="text/javascript">
     (function(d, m){
-        var kommunicateSettings = 
-            {"appId":"","popupWidget":true,"automaticChatOpenOnNavigation":true,
- "labels": {
+        var kommunicateSettings = {"appId":"39cba992edc99a4c67893527fb0d9bd3c","popupWidget":true,"automaticChatOpenOnNavigation":true,
+        "labels": {
           'emoji.hover.text': {
             'poor': 'Mala',
             'great': 'Buena',
@@ -12,6 +11,10 @@
           'conversation.header.dropdown': {
             'CSAT_RATING_TEXT': 'Califica esta conversación'
         },
+        'lead.collection': {
+          'title': 'Formulario ',
+          'submit': 'Iniciar Conversación',
+      },
           'csat.rating': {
             'CONVERSATION_RATED': 'Tu calificación es:',
             'RATE_CONVERSATION': 'Califica tu conversación',
@@ -27,13 +30,11 @@
         'typing': 'escribiendo...',
         'is.typing': 'está escribiendo',
       },
-
         "onInit": function() {
-let css = ".km-custom-widget-stroke {stroke: black !important;fill: white !important;}.km-cta-multi-button-container button.km-cta-button {font-size: 14px;border: 3px solid #000;background-color: #7ddf6c;color: black !important;} .mck-msg-left .mck-msg-box a {color: #2fdaca !important;text-decoration: underline;}.mck-msg-left .mck-msg-box a:hover {color: #da3f2f !important;}.km-custom-widget-background-color{background-color: #DEFB09!important;color: black!important;}.mck-msg-left .mck-msg-box{background-color: black!important;color:#DEFB09!important;}.mck-msg-right .mck-msg-box{background-color: black!important;}.km-cta-multi-button-container button.km-quick-replies{border: 3px solid #000;border-top-color: rgb(0, 0, 0);border-right-color: rgb(0, 0, 0);border-bottom-color: rgb(0, 0, 0);border-left-color: rgb(0, 0, 0);background-color: #7ddf6c;color: black !important;}.mck-msg-list-icon{fill: black;}.mck-sidebox .mck-box-title{color: black!important;}.mck-tab-title{color: black!important;}.mck-agent-status-text{color: black!important;}.mck-box-top {background-color: #DEFB09!important;color: black!important;}.mck-msg-left .mck-msg-box{background-color: black!important;color:#DEFB09!important;}.mck-msg-right .mck-msg-box{background-color: black!important;color:white!important;}";
+          let css = ".km-custom-widget-stroke {stroke: black !important;fill: white !important;}.km-cta-multi-button-container button.km-cta-button {font-size: 14px;border: 3px solid #000;background-color: #7ddf6c;color: black !important;} .mck-msg-left .mck-msg-box a {color: #2fdaca !important;text-decoration: underline;}.mck-msg-left .mck-msg-box a:hover {color: #da3f2f !important;}.km-custom-widget-background-color{background-color: #DEFB09!important;color: black!important;}.mck-msg-left .mck-msg-box{background-color: black!important;color:#DEFB09!important;}.mck-msg-right .mck-msg-box{background-color: black!important;}.km-cta-multi-button-container button.km-quick-replies{border: 3px solid #000;border-top-color: rgb(0, 0, 0);border-right-color: rgb(0, 0, 0);border-bottom-color: rgb(0, 0, 0);border-left-color: rgb(0, 0, 0);background-color: #7ddf6c;color: black !important;}.mck-msg-list-icon{fill: black;}.mck-sidebox .mck-box-title{color: black!important;}.mck-tab-title{color: black!important;}.mck-agent-status-text{color: black!important;}.mck-box-top {background-color: #DEFB09!important;color: black!important;}.mck-msg-left .mck-msg-box{background-color: black!important;color:#DEFB09!important;}.mck-msg-right .mck-msg-box{background-color: black!important;color:white!important;}";
           
           window.Kommunicate.customizeWidgetCss(css);
-        }
-};
+        }};
         var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
         s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
         var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
