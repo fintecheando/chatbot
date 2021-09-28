3983bb3698ae1393d3a5eac6fbf744d7a94047f2
Gustavo Espíndola <36246104+gustavao@users.noreply.github.com>
2021-09-28 12:11:05 -0500

    Update widget.js

--- widget.js
+++ widget.js
@@ -1,6 +1,5 @@
-<script type="text/javascript">
     (function(d, m){
-        var kommunicateSettings = {"appId":"Your ApiKey","popupWidget":true,"automaticChatOpenOnNavigation":true,
+        var kommunicateSettings = {"appId":"your_appid","popupWidget":true,"automaticChatOpenOnNavigation":true,
         "labels": {
           'emoji.hover.text': {
             'poor': 'Mala',
@@ -29,16 +28,27 @@
         'conversations.title': 'Conversaciones',
         'typing': 'escribiendo...',
         'is.typing': 'está escribiendo',
+        'waiting.queue.message': {
+          'contact.name': 'En Espera...',
+          'header.text': 'En espera...',
+          'first.Part': 'A la brevedad te atenderemos, eres el',
+          'last.part': ' en la lista de atención.'
+      }
       },
         "onInit": function() {
-          let css = ".km-custom-widget-stroke {stroke: black !important;fill: white !important;}.km-cta-multi-button-container button.km-cta-button {font-size: 14px;border: 3px solid #000;background-color: #7ddf6c;color: black !important;} .mck-msg-left .mck-msg-box a {color: #2fdaca !important;text-decoration: underline;}.mck-msg-left .mck-msg-box a:hover {color: #da3f2f !important;}.km-custom-widget-background-color{background-color: #DEFB09!important;color: black!important;}.mck-msg-left .mck-msg-box{background-color: black!important;color:#DEFB09!important;}.mck-msg-right .mck-msg-box{background-color: black!important;}.km-cta-multi-button-container button.km-quick-replies{border: 3px solid #000;border-top-color: rgb(0, 0, 0);border-right-color: rgb(0, 0, 0);border-bottom-color: rgb(0, 0, 0);border-left-color: rgb(0, 0, 0);background-color: #7ddf6c;color: black !important;}.mck-msg-list-icon{fill: black;}.mck-sidebox .mck-box-title{color: black!important;}.mck-tab-title{color: black!important;}.mck-agent-status-text{color: black!important;}.mck-box-top {background-color: #DEFB09!important;color: black!important;}.mck-msg-left .mck-msg-box{background-color: black!important;color:#DEFB09!important;}.mck-msg-right .mck-msg-box{background-color: black!important;color:white!important;}";
+          let css = "#mck-submit-comment{background: #defb09 !important;color: black !important;border: 2px solid #7ddf6c !important;width: 80%;border-radius: 30px; font-weight: bold;}.km-csat-skeleton .mck-conversation-comment{font-weight: bold;}#mck-msg-new{background-color: #defb09 !important;border: 2px solid #7ddf6c !important;color: black !important;}.km-modal input, .km-modal select, .km-modal textarea{border-radius: 50px;}.km-start-conversation{color: black !important; border-color: #7ddf6c !important;}.km-custom-widget-stroke {stroke: black !important;fill: white !important;}.km-cta-multi-button-container button.km-cta-button {font-size: 14px;border: 3px solid #000;background-color: #7ddf6c;color: black !important;} .mck-msg-left .mck-msg-box a {color: #2fdaca !important;text-decoration: underline;}.mck-msg-left .mck-msg-box a:hover {color: #da3f2f !important;}.km-sidemodal--modal-header{background-color: #DEFB09!important;color: black!important;}.mck-msg-left .mck-msg-box{background-color: black!important;color:#DEFB09!important;}.mck-msg-right .mck-msg-box{background-color: black!important;}.km-cta-multi-button-container button.km-quick-replies{font-weight: bold;border: 3px solid #defb09 !important;border-top-color: rgb(0, 0, 0);border-right-color: rgb(0, 0, 0);border-bottom-color: rgb(0, 0, 0);border-left-color: rgb(0, 0, 0);background-color: #7ddf6c;color: black !important;}.mck-msg-list-icon{fill: black;}.mck-sidebox .mck-box-title{color: black!important;}.mck-tab-title{color: black!important;}.mck-agent-status-text{color: black!important;}.mck-box-top {background-color: #DEFB09!important;color: black!important;}.mck-msg-left .mck-msg-box{background-color: black!important;color:#DEFB09!important;}.mck-msg-right .mck-msg-box{background-color: black!important;color:white!important;}";
           
           window.Kommunicate.customizeWidgetCss(css);
         }};
-        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        
+        var s = document.createElement("script"); 
+        s.type = "text/javascript"; 
+        s.async = true;
         s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-        window.kommunicate = m; m._globals = kommunicateSettings;
+        var h = document.getElementsByTagName("head")[0]; 
+        h.appendChild(s);
+        window.kommunicate = m; 
+        m._globals = kommunicateSettings;
     })(document, window.kommunicate || {});
-/* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
-</script>
+
+    setTimeout(()=>{ this.active = true; }, 3000);
